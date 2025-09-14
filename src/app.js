fccd5468e2877215e35240bf6f6de44547489688
Saurabh Tripathi <saurabh8840@gmail.com>
2025-09-14 23:50:18 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
     credentials:true
 }))
 
-// app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.static("public"))
 app.use(cookieParsar())
