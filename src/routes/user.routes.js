f0944fdc842997600f2d7be56bd908494bd0b8ab
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-09-20 23:43:25 +0530

    new route added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import {Router} from "express"
+import { upload } from "../middlewares/multer.middleware";
 
 const router=Router();
 
-router.route('/login').post(loginUser)
+router.route('/login').post(
+    
+    upload.fields([
+        {
+            name:'avatar',maxCount:1
+        },
+        {
+            name:'coverimage',maxcount:1
+        }
+    ]),
+   
+    loginUser
+)
 
 
  export default router;
