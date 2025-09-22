040453d155b908820668781f1b9bc11b80d845f9
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-09-22 23:48:58 +0530

    updated user route

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ import { DB_NAME } from "../constant";
 const connectDB=async()=>{
 
     try {
-         const mongooseInstace=await mongoose.connect(`/{DB_NAME}`);
+         const mongooseInstace=await mongoose.connect(`{process.env.MONGODB_URL}/{DB_NAME}`);
          console.log(`\n MongoDb connected successfully`);
     } catch (error) {
         console.log("Mongodb connection failed",error);
@@ -18,3 +18,4 @@ const connectDB=async()=>{
 }
 
 export default connectDB;
+
