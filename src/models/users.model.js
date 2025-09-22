1862fb19ca676ab49541a2a86cbe4245b1d6d18f
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-09-22 23:50:06 +0530

    added password check and jwt generator

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,5 +1,16 @@
 import mongoose, { Schema } from "mongoose"
 
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:'./.env'
+})
+
+
+import jwt from "jsonwebtoken"
+
+import bcrypt from "bcrypt"
+
 const User=new  Schema({
 
     username:{
@@ -37,7 +48,59 @@ const User=new  Schema({
         required:[true,'Password is required']
         
     },
+    refreshToken:{
+        type:String
+    }
     
 },{timestamps:true})
 
+
+//hashing password 
+
+userSchema.pre("save",async function(){
+    
+    if(!this.isModified("Password"))return next();
+
+    this.password=await bcrypt.hash(this.password,10)
+    next()
+})
+
+//compareing password 
+userSchema.methods.isPasswordCorrect=async function(password){
+
+    return await bcrypt.compare(password,this.password)
+
+}
+
+
+//generate accesstoken 
+
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+        {
+        id:this._id
+        },
+       
+        process.env.ACCESS_TOKEN_SECRET
+       ,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+
+
+//generate refreshtoken 
+
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign(
+        {
+        id:this._id
+        },
+       
+        process.env.REFRESH_TOKEN_SECRET
+       ,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    })
+}
 export default User;
