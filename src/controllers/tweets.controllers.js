1fea709c05885fb3d768742502eec19e27b2ff0a
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-09-27 16:56:42 +0530

    adde tweets controller

--- src/controllers/tweets.controllers.js
+++ src/controllers/tweets.controllers.js
@@ -32,11 +32,99 @@ const createTweet=asyncHandler(async(req,res)=>{
 })
 
 const getUserTweet=asyncHandler(async(req,res)=>{
+     
+    const {userId}=req.params;
+
+    if(!userId){
+        throw new ApiError(400,"user id is required ");
+    }
+    //i have to get user my userid
+    const tweet=await Tweet.find({owner:userId});
+
+    if(!tweet || tweet.length===0){
+        throw new ApiError(400,"tweet not found")
+
+    }
 
     
 
+    return res
+       .status(200)
+       .json(new ApiResponse( 
+        200,
+        tweet,
+        "user tweet fetched usccessfullly "
+       ))
+    
+
+});
+
+const updateTweet=asyncHandler(async(req,res)=>{
+
+    const {tweetId}=req.params;
+    const {updatedcontent}=req.body;
+
+    
+    if(!tweetId){
+        throw new ApiError(401,"tweet not found")
+    }
+
+    if(!updatedcontent || updatedcontent.trim()===""){
+        throw new ApiError(400,"No input found")
+    }
+
+    const tweet=await Tweet.findOneAndUpdate(
+        {_id:tweetId,owner:req.user._id},
+        {
+            $set:{
+                content:updatedcontent
+            }
+        },
+        {new :true,runValidators: true }
+    )
+
+    if(!tweet){
+        throw new ApiError(404,"Tweet not found or you are not the owner ");
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+            200,
+            tweet,
+            "tweet updated successfully"
+        )
+      );
 })
 
+const deleteTweet=asyncHandler(async(req,res)=>{
+
+    const {tweetId}=req.params;
+
+    if(!tweetId){
+        throw new ApiError(400,"tweet not found")
+    }
+
+    const deletedTweet=await Tweet.deleteOne({_id:tweetId,owner:req.user._id});
+    
+    if (deletedTweet.deletedCount === 0){
+    throw new ApiError(404, "Tweet not found or you are not the owner");
+    }
+ 
+
+    return res
+       .status(200)
+       .json(
+        new ApiResponse(200,{},"tweet deleted successfully")
+       )
+ })
+
+
+
 export {
-    createTweet
+    createTweet,
+    getUserTweet,
+    updateTweet,
+    deleteTweet
 }
