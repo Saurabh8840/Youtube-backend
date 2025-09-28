d4de45771b852702939b69738cf7874890cdb421
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-09-28 21:35:07 +0530

    adde subscription

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -5,4 +5,20 @@ const router=Router();
 app.use(verifyjwt);
 
 
+///here i have to think about route which 
+// i have 3 things 
+// 1.toggle subscription (subscribe and unsubscribe)
+// 2.get all detail of channel subscribeed by me
+// 3.get all details of all subscribe of me
+
+
+route.route("c/:channelId").get(getSubscribedChannels)
+router.route("c/:channelId").get(toggleSubscription);
+
+router.route("/u/:subscribedId").get(getUserChannelSubscribers);
+
+export default router
+
+
+
 
