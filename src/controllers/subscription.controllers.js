47872b53cc3fb91de1e4c14926ee904528fb0c78
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-09-29 17:49:03 +0530

    aded togglesubscription

--- src/controllers/subscription.controllers.js
+++ src/controllers/subscription.controllers.js
@@ -1,5 +1,71 @@
 import asyncHandler from "../utils/asynchandler.js"
 
+
+const toggleSubscription=asyncHandler(async(req,res)=>{
+
+    const {channelId}=req.params;
+
+    //toggle subscription 
+
+    if(!channelId){
+        throw new ApiError(400,"channel Id is required")
+    }
+
+    //find user id 
+    const userId=req.user._id;
+
+
+
+    //now check if existing int table 
+    const existingSubscription=await Subscription.findOne({
+        channel:channelId,
+        subscriber:userId
+    })
+     
+    if(existingSubscription){
+
+        await existingSubscription.deleteOne()
+
+        return res
+          .status(200)
+          .json(
+            new ApiResponse(200,{subscribed:false},"Unsubscribed successfully");
+
+          )
+        
+    }
+
+    else{
+
+        const newSubscriber=await Subscription.create({
+            subscriber:userId,
+            channel:channelId
+        })
+
+        if(!newSubscriber){
+            throw new ApiError(400,"unable to subscribe")
+        }
+
+        return res
+       .status(201)
+       .json(new ApiResponse(201,{subscribed:true},"subscribed successfully"))
+
+    }
+
+    
+})
+
+
+
+//controller to return subscriber list of a channel
+const getUserChannelSubscribers=asyncHandler(async(req,res)=>{
+
+    //i have to count subscription 
+    
+    
+})
+
+
 const getSubscribedChannels=asyncHandler(async(req,res)=>{
     
     const {subscribedId}=req.params;
@@ -10,21 +76,9 @@ const getSubscribedChannels=asyncHandler(async(req,res)=>{
 
     const subscriberCount=await Subscription.countDocuments({subscriber:subscriberId});
 
-    if(!subcriberCount){
-        throw new ApiError(400,"error while counting subscriber")
-    }
-
-    return res
-        .status(200)
-        .json(new ApiError(200,subscriberCount,,"fetched successfully"))
-
+    
     
 })
 
 
 
-const getUserChannelSubscribers=asyncHandler(async(req,res)=>{
-
-    //i have to count subscription 
-    
-})
