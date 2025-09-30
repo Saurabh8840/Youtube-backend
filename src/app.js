12a872dfc50cad52df894f68874f9f29ad75c9b4
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-09-30 23:52:46 +0530

    added new update

--- src/app.js
+++ src/app.js
@@ -21,9 +21,12 @@ export {app}
 import userRouter from "./routes/user.routes.js";
 import healthCheckRouter from "./routes/healthCheck.route.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import videosRouter from "./routes/videoRouter.js"
 
 //routes declarartion 
 app.use("/api/v1/healthCheck",healthCheckRouter)
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/tweets",tweetRouter)
 app.use("/api/v1/subscriptions",subscriptionRouter)
+app.use("/api/v1/videos",videoRouter)
